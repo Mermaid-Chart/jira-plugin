40415c88c632cd3a2985c42bd6939474c9551008
Nikita <nnovitsky@tula.co>
2024-06-03 15:41:29 +0300

    Add new files

---
+++ public/js/select.js
@@ -0,0 +1,45 @@
+import { h, render } from "https://esm.sh/preact";
+import { useState } from "https://esm.sh/preact/hooks";
+import htm from "https://esm.sh/htm";
+
+const html = htm.bind(h);
+
+function App() {
+  const [accessToken, setAccessToken] = useState(mcAccessToken);
+  const [user, setUser] = useState(loggedUser);
+
+  AP.dialog.getCustomData(function (customData) {
+    console.log("customData");
+    console.log(customData);
+  });
+
+  // if (!accessToken) {
+  //   return html` <${Login} onLogin="${onLogin}" /> `;
+  // }
+
+  const buildUrl = (pathname) => {
+    return `${MC_BASE_URL}/oauth/frame/?token=${mcAccessToken}&redirect=${pathname}`;
+  };
+
+  // buildUrl(
+  //   `/app/projects/${document.projectID}/diagrams/${document.documentID}/version/v.${document.major}.${document.minor}/edit`
+  // )
+
+  const iframeURL = buildUrl(`/app/plugins/confluence/select`);
+  console.log("buildUrl:", buildUrl);
+
+  if (iframeURL) {
+    //<iframe src="${iframeURL}" name="${JSON.stringify(iframeData)}" />
+    return html` <iframe src="${iframeURL}" name="" /> `;
+  }
+
+  // return html`
+  //   <${Form}
+  //     user="${user}"
+  //     onLogout="${onLogout}"
+  //     mcAccessToken="${accessToken}"
+  //   />
+  // `;
+}
+
+render(html` <${App} />`, document.getElementById("editor-content"));
