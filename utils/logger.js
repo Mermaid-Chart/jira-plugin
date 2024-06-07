4dfc09ce57600444995661a6f9869ad22f233962
Evgeny <ezubarev@tula.co>
2024-06-07 15:54:21 +0200

    Add logger

--- utils/logger.js
+++ utils/logger.js
@@ -10,8 +10,8 @@ const logger = winston.createLogger({
     // using environment variables like `AXIOM_DATASET` and `AXIOM_TOKEN`
     new AxiomTransport({
       dataset: "mc-jira-plugin",
-      token: "xaat-4f79ec61-c2ad-49b7-82bb-4d73b92b4a32",
-      orgId: "mc-plugins-luip",
+      token: "xaat-96973040-e16f-44b4-b1e9-f2fba86a835b",
+      orgId: "tulaco-uwn4",
     }),
   ],
 });
