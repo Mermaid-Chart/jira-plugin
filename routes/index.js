988cb340bdad564a973a987a224c2d3c63c36684
Evgeny <ezubarev@tula.co>
2024-06-14 13:56:35 +0200

    Disable mermaidcharts urls

--- routes/index.js
+++ routes/index.js
@@ -68,28 +68,26 @@ export default function routes(app, addon) {
       log.error("error_charts: ", e);
     }
 
-    try {
-      for (let index = 0; index < charts.length; ++index) {
-        const chart = charts[index];
-        // chart.diagramImage = await mermaidAPI.getDocumentAsPng(
-        //   chart,
-        //   access_token
-        // );
-
-        const attachment = await getJiraIssueAttachment(
-          req.context.http,
-          issueKey,
-          chart.attachmentId
-        );
-
-        //Fallback to MermaidCharts url instead of jira issue attachment
-        if (!attachment) {
-          chart.diagramUrl = mermaidAPI.getDocumentAsPngUrl(chart);
-        }
-      }
-    } catch (e) {
-      log.error("error getting pngs: ", e);
-    }
+    // try {
+    //   for (let index = 0; index < charts.length; ++index) {
+    //     const chart = charts[index];
+    //     // chart.diagramImage = await mermaidAPI.getDocumentAsPng(
+    //     //   chart,
+    //     //   access_token
+    //     // );
+    //     const attachment = await getJiraIssueAttachment(
+    //       req.context.http,
+    //       issueKey,
+    //       chart.attachmentId
+    //     );
+    //     //Fallback to MermaidCharts url instead of jira issue attachment
+    //     if (!attachment) {
+    //       chart.diagramUrl = mermaidAPI.getDocumentAsPngUrl(chart);
+    //     }
+    //   }
+    // } catch (e) {
+    //   log.error("error getting pngs: ", e);
+    // }
 
     const auth = user ? {} : await mermaidAPI.getAuthorizationData();
     // const auth = { url: "", state: "" };
