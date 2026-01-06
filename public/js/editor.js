1e9864bfdd231250ffdb97df468c93524aaaa34d
Almoizs-1310 <almoiz@mermaidchart.com>
2026-01-06 13:20:06 +0530

    Updated jira plugin to support fullscreen and add pluginSource in the url

--- public/js/editor.js
+++ public/js/editor.js
@@ -41,6 +41,9 @@ function App() {
           AP.dialog.close({ chart: e.data.data, replace: true });
           break;
       }
+      if (e.data.type === "mermaid-chart-jira-back" && action === "navigateBack") {
+        AP.dialog.close();
+      }
     };
   }, []);
 
@@ -61,7 +64,7 @@ function App() {
   };
 
   const iframeURL = buildUrl(
-    `/app/projects/${image.projectID}/diagrams/${image.documentID}/version/v.${image.major}.${image.minor}/edit`
+      `/app/projects/${image.projectID}/diagrams/${image.documentID}/version/v${image.major}.${image.minor}/edit?pluginSource=jira`
   );
 
   if (iframeURL) {
