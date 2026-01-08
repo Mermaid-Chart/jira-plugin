ef21847d30af5284d31a54a7fb5e753b55936d17
Almoizs-1310 <almoiz@mermaidchart.com>
2026-01-08 14:04:27 +0530

    Revert "MC-4115 Added UTM Source and UTM Medium for Jira Plugin  and integrat…"

--- public/js/issue-content.js
+++ public/js/issue-content.js
@@ -1,7 +1,6 @@
 import { h, render, Fragment } from "https://esm.sh/preact";
 import { useState } from "https://esm.sh/preact/hooks";
 import htm from "https://esm.sh/htm";
-import analytics from "./analytics.js";
 const html = htm.bind(h);
 let timeout;
 
@@ -26,7 +25,6 @@ function App() {
     });
     setAccessToken(undefined);
     setUser(null);
-    analytics.trackLogout()
     hideLoadingAnimation();
     window.location.reload();
   };
@@ -43,7 +41,6 @@ function App() {
 
     const windowObjectReference = window.open(loginURL, "loginWindow", options);
     windowObjectReference.focus();
-    analytics.trackConnectToMermaidChart();
 
     const callback = async () => {
       const res = await fetch(`/check_token?state=${loginState}`, {
@@ -85,7 +82,7 @@ function App() {
       connectToMermaidClick();
       return;
     }
-    analytics.trackPluginDiagramEdit();
+
     AP.dialog.create({
       key: "dialog-module-edit",
       chrome: true,
